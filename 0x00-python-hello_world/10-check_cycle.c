31875c7f13627a07403d9e7eb607501d8ab5a1fa
Abdel Mejia <bhalut.bear@gmail.com>
2020-04-27 20:57:21 +0000

    Add lists.h and 10-check_cycle.c files 0x00-python-hello_world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - hat checks if a singly linked list has a cycle in it
+ * @list: singly list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	int i = 0;
+
+	current = list;
+	while (current != NULL)
+	{
+		current = current->next;
+		if (current == list)
+		{
+			i = 1;
+			break;
+		}
+	}
+
+	return (i);
+}
