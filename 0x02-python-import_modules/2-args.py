1c74d5100bf5b60435dc7808eb4e70ba8a22dcd7
Abdel Mejia <bhalut.bear@gmail.com>
2020-04-30 22:38:36 +0000

    Add 2-args.py file 0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,14 @@
 if __name__ == "__main__":
     import sys
 
-if len(sys.argv) > 1:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-    for index in range(1, len(sys.argv)):
-        print("{:d}: {}".format(index, sys.argv[index]))
+arg = sys.argv
+
+if len(arg) is 2:
+    print("{:d} argument.".format(len(arg) - 1))
+    print("{:d}: {}".format(len(arg) - 1, arg[len(arg) - 1]))
+elif len(arg) > 2:
+    print("{:d} arguments:".format(len(arg) - 1))
+    for index in range(1, len(arg)):
+        print("{:d}: {}".format(index, arg[index]))
 else:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
+    print("{:d} arguments.".format(len(arg) - 1))
