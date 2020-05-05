ef3b373e58bb92c745f66611376be43a9a983501
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-05 18:06:33 +0000

    Add 1-elemtent_at.py file 0x03-python-data_structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
     else:
-        for i in range(len(my_list) - 1):
+        for i in range(0, len(my_list)):
             if i == idx:
                 return int(my_list[i])
