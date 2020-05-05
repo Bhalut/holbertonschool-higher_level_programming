69ea355e41caaf41719a214d173f2ec4da44b95c
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-05 23:52:26 +0000

    Add 13-is_palindrome.c file 0x03-python-data_structures

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+void reverse(listint_t **head)
+{
+	listint_t *current = *head, *next = NULL, *prev = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *rev = *head, *tmp = *head;
+
+	reverse(&rev);
+
+	while (tmp->next != NULL)
+	{
+		if (rev->n != tmp->n)
+			return (0);
+
+		rev = rev->next;
+		tmp = tmp->next;
+	}
+
+	return (1);
+}
