8c4f6cdfa541b651c571f3771973d6a657e2a8d2
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-08 04:28:13 +0000

    Add 12-roman_to_int.py file 0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    number = 0
+    number = maximun = 0
 
-    for num in range(len(roman_string)):
-        if (
-            num > 0 and
-            roman[roman_string[num]] > roman[roman_string[num - 1]]
-        ):
-            number += roman[roman_string[num]] - \
-                (2 * roman[roman_string[num - 1]])
-        else:
+    for num in range(len(roman_string) - 1, -1, -1):
+        if roman[roman_string[num]] >= maximun:
             number += roman[roman_string[num]]
+        else:
+            number -= roman[roman_string[num]]
+
+        maximun = roman[roman_string[num]]
 
     return number
