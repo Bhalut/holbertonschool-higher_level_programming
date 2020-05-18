f54d3a49b80610283d5619a0b4e875737aa807e0
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-18 19:29:18 +0000

    Add 0-safe_print_integer file at 0x05-python-exceptions project

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
+            count += 1
     except IndexError:
         x = length(my_list)
     finally:
         print("")
 
-    return my_list[x - 1]
+    return count
 
 
 def length(my_list=[]):
