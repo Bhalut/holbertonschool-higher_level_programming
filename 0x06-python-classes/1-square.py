26de4b8c28c64120cd3c55bc80020f8c826da7ba
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-19 18:15:01 +0000

    Add 1-square file at 0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square():
+    """Square Class
+
+    Class with private attribute size
+    """
+
+    def __init__(self, size):
+        """__init__ Contructor
+
+        Contructor of class Square
+
+        Args:
+            size (integer): privete attribute for zise Square
+        """
+        self.__size = size
