4a69274d3c7008968b929e41080093dc1686546a
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-19 18:19:53 +0000

    Add 2-square file at 0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 class Square():
+    """Square Class
+
+    Class Square with validation attribute
+    """
+
     def __init__(self, size=0):
+        """__init__ Constructor
+
+        Contructor with size attribute
+
+        Args:
+            size (int, optional): size of Square. Defaults to 0.
+
+        Raises:
+            TypeError: Error by not be type integer
+            ValueError: Error by be negative number
+        """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
