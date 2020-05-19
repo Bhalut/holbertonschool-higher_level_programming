6ac499317b754e9cda861fad0efd335e2e074386
Abdel Mejia <bhalut.bear@gmail.com>
2020-05-19 22:07:35 +0000

    Add 6-square file at 0x06-python-classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,6 +8,7 @@
     Raises:
         TypeError: Error by not be type integer
         ValueError: Error by be negative number
+        TypeError: position must be a tuple of 2 positive integers
 
     Returns:
         integer: Square Area (size * size)
@@ -18,11 +19,10 @@ class Square():
     """Square Class
 
     Class Square with public method (Area)
-    Getter - Setter for size attribute
-    Print in stdout the square
+    and getter - setter for size attribute
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """__init__ Constructor
 
         Contructor with size attribute
@@ -31,6 +31,7 @@ class Square():
             size (int, optional): size of Square. Defaults to 0.
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -64,6 +65,36 @@ class Square():
 
         self.__size = value
 
+    @property
+    def position(self):
+        """position Getter
+
+        Get position valut
+
+        Returns:
+            tuple: position square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """position Setter
+
+        Set position square
+
+        Args:
+            value (tuple): position square
+
+        Raises:
+            TypeError: position must be a tuple of 2 positive integers
+        """
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """area Method
 
@@ -83,6 +114,9 @@ class Square():
             print()
             return
 
-        for x in range(self.__size):
-            [print("#", end="") for y in range(self.__size)]
+        [print() for i in range(0, self.__position[1])]
+
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print()
