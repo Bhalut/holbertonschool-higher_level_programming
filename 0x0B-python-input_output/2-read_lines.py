06fac1368d0f3d3b11f4cdd01f2a6f3a43857b96
Abdel Mejia <bhalut.bear@gmail.com>
2020-06-08 04:41:19 +0000

    Add 2-read_lines.py file at 0x0B-python-input_output

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+""" 2-read_lines.py
+
+    read_lines func
+"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """read_lines func
+
+    reads n lines of a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str, optional): file. Defaults to "".
+        nb_lines (int, optional): number of lines. Defaults to 0.
+    """
     with open(file=filename, mode="r", encoding="utf8") as f:
         if nb_lines is not 0:
             for line in range(nb_lines):
