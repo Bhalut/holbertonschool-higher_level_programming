abdee21052f13473b723adaeca5b162687fa14a0
Abdel Mejia <bhalut.bear@gmail.com>
2020-06-08 04:44:05 +0000

    Add 2-read_lines.py file at 0x0B-python-input_output

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -15,9 +15,10 @@ def read_lines(filename="", nb_lines=0):
         nb_lines (int, optional): number of lines. Defaults to 0.
     """
     with open(file=filename, mode="r", encoding="utf8") as f:
-        if nb_lines is not 0:
-            for line in range(nb_lines):
-                print(f.readline(), end="")
-        else:
+        if nb_lines == 0:
             for line in f:
                 print(line, end="")
+
+        if nb_lines > 0:
+            for line in range(nb_lines):
+                print(f.readline(), end="")
