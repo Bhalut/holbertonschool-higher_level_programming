7eef3b76027b51dac013880fa15d5c507fe1f1d7
Abdel Mejia <bhalut.bear@gmail.com>
2020-06-09 17:48:55 +0000

    Add 9-add_item.py file at 0x0B-python-input_output

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import sys
+""" 9-add_item.py
 
+    adds all arguments to a Python list, and then save them to a file
+"""
+import sys
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
 
 if __name__ == "__main__":
     filename = "add_item.json"
