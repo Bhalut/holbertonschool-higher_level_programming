e2ad17b44975b0825464a7d6cf7e1b13ed5664c3
Abdel Mejia <bhalut.bear@gmail.com>
2020-07-14 21:28:47 +0000

    Add 0-privileges.sql file at 0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- on your server (in localhost).
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL ON *.* TO 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
