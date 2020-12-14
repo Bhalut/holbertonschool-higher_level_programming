79eeea41b29fc62632335db089d099d0dec2a8a2
Abdel Mejia <bhalut.bear@gmail.com>
2020-12-14 03:35:33 +0000

    Add 3-my_safe_filter_states.py at 0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ 3-my_safe_filter_states.py
 
-    write a script that takes in arguments and displays all values
-    in the states table of hbtn_0e_0_usa where name matches the argument.
+    takes in arguments and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import sys
