e88f02f421fa592df0d75ef752b1194744bb295b
Abdel Mejia <bhalut.bear@gmail.com>
2020-12-14 03:50:37 +0000

    Add 4-cities_by_state.py at 0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -23,8 +23,8 @@ def main():
 
     cursor = db.cursor()
     cursor.execute(
-        """SELECT cities.id, cities.name, states.name
-        FROM cities LEFT JOIN states ON cities.state_id = states.id;""")
+        """SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id;""")
 
     for row in cursor.fetchall():
         print(row)
