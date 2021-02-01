c4ec6897dadabb517594649411786cd6577b958c
Abdel Mejia <bhalut.bear@gmail.com>
2021-02-01 03:34:55 +0000

    Add 6-peak.py file at 0x10-python-network_0

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 6-peak
+
+    function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """Function max integer
+
+    Args:
+        list_of_integers (list)
+
+    Returns:
+        Max integer or None
+    """
+    if list_of_integers:
+        return max(list_of_integers)
+    else:
+        return None
